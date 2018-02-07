f59c5d8f337e6c2cbc3ad42f4720385cedeedebb
Christoph Schulz <schulzcbs@gmail.com>
2018-02-07 14:56:03 +0100

    Fix TypeScript definitions

--- dist/fuzzy-dbscan.d.ts
+++ dist/fuzzy-dbscan.d.ts
@@ -1,19 +1,26 @@
-export declare const enum Category {
-    Core = "CORE",
-    Border = "BORDER",
-    Noise = "NOISE",
-}
-export interface Assignment {
-    index: number;
-    category: Category;
-    label: number;
-}
-export declare type DistanceFunction = (a: any, b: any) => number;
-export declare class FuzzyDBSCAN {
-    epsMin(value?: number): FuzzyDBSCAN | number;
-    epsMax(value?: number): FuzzyDBSCAN | number;
-    mPtsMin(value?: number): FuzzyDBSCAN | number;
-    mPtsMax(value?: number): FuzzyDBSCAN | number;
-    distanceFn(fn?: DistanceFunction): FuzzyDBSCAN | DistanceFunction;
-    cluster(points: Array<any>): Array<Array<Assignment>>;
+export = fuzzy_dbscan;
+
+declare function fuzzy_dbscan(): fuzzy_dbscan.FuzzyDBSCAN;
+
+declare namespace fuzzy_dbscan {
+    const enum Category {
+        Core = "CORE",
+        Border = "BORDER",
+        Noise = "NOISE",
+    }
+    interface Assignment {
+        index: number;
+        category: Category;
+        label: number;
+    }
+    type DistanceFunction = (a: any, b: any) => number;
+    
+    class FuzzyDBSCAN {
+        epsMin(value?: number): FuzzyDBSCAN | number;
+        epsMax(value?: number): FuzzyDBSCAN | number;
+        mPtsMin(value?: number): FuzzyDBSCAN | number;
+        mPtsMax(value?: number): FuzzyDBSCAN | number;
+        distanceFn(fn?: DistanceFunction): FuzzyDBSCAN | DistanceFunction;
+        cluster(points: Array<any>): Array<Array<Assignment>>;
+    }    
 }
